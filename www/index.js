1da357e7fb09179001a22418061b896326b7da50
Richard Shea <rshea@thecubagroup.com>
2018-06-04 17:03:43 +1200

    Tidy up  JS

--- www/index.js
+++ www/index.js
@@ -1,24 +1,12 @@
 var TCG = TCG || function () { };
 TCG.LIL = function () {
-    _countStates = function () {
-      alert("This is _countStates");
-    }
-    _buildData = function () {
-      alert("This is _buildData");
+    _bar= function () {
+      alert("This is bar");
     }
     return {
       //PUBLIC AREA
-      boardstates : null,
-      boardstateslength : null,
-      boardidx : 0,
-      intHandle : null, 
-      playerFundsChart : null,
-      playerPropHldngChart : null,
-
-      foo: function () {
-      },
       pageElementsInitialization : function () {
-        alert("This is pageElementsInitialization");
+        alert("This is pageElementsInitialization. v1");
       },
   };
 }();
