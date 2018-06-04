2253ca9438a95169af4e422b7114f0598ff4ec7a
Richard Shea <rshea@thecubagroup.com>
2018-06-04 17:01:48 +1200

    Build js structure to hold references to each piece of svg

--- lilysrc/builder.py
+++ lilysrc/builder.py
@@ -1,3 +1,5 @@
+import pprint
+
 HTMLFRAG = '''
     <div id="{0}">
     <object type="image/svg+xml" data="{1}.svg">
@@ -8,17 +10,26 @@ HTMLFRAG = '''
     </div>
 
 '''
-for oct in ['0','1']:
-    for pitch in ['c','d','e','f','g', 'a', 'b']:
-        divid = '''octave-{0}-pitch-{1}'''.format(oct, pitch)
-        n='''{0}+{1}'''.format(pitch, oct)
-        command = '''/home/rshea/bin/lilypond -dbackend=svg {0}.ly;mv {0}.svg ../www'''.format(n)
-        print(command)
-print("")
-for oct in ['0','1']:
-    for pitch in ['c','d','e','f','g', 'a','b']:
-        divid = '''octave-{0}-pitch-{1}'''.format(oct, pitch)
-        n='''{0}+{1}'''.format(pitch, oct)
-        htmlfrag = HTMLFRAG.format(divid, n)
-        print(htmlfrag)
-        print('''<!--- --->''')
+def main():
+    for oct in ['0','1']:
+        for pitch in ['c','d','e','f','g', 'a', 'b']:
+            divid = '''octave-{0}-pitch-{1}'''.format(oct, pitch)
+            n='''{0}+{1}'''.format(pitch, oct)
+            command = '''/home/rshea/bin/lilypond -dbackend=svg {0}.ly;mv {0}.svg ../www'''.format(n)
+            print(command)
+    print("")
+    arrforjs = []
+    for oct in ['0','1']:
+        for pitch in ['c','d','e','f','g', 'a','b']:
+            divid = '''octave-{0}-pitch-{1}'''.format(oct, pitch)
+            n='''{0}+{1}'''.format(pitch, oct)
+            htmlfrag = HTMLFRAG.format(divid, n)
+            print(htmlfrag)
+            print('''<!--- --->''')
+            arrforjs.append(divid)
+    print("")
+    pprint.pprint(arrforjs)
+    print("")
+
+if __name__ == "__main__":
+    main()
