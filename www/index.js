4c9187e252977b9b3d066e7cd4e3161d5e191420
Richard Shea <rshea@thecubagroup.com>
2018-06-04 23:24:35 +1200

    Now working with acclerator keys

--- www/index.js
+++ www/index.js
@@ -24,56 +24,85 @@ TCG.LIL = function () {
     }
     function _start(){
       console.log( "Handler for startquestions .click() called." );
+      $('#startdiv').removeClass( "startdivvisible" )
+      $('#startdiv').addClass( "startdivinvisible" )
     }
-    function _moveToNextQuestion(){
+    function _nextButtonClickHandler(){
       if (TCG.LIL.currentQuestion != "")
       {
+        //Hide current question
         $('#' + TCG.LIL.currentQuestion).removeClass( "noteattemptcardvisible" )
         $('#' + TCG.LIL.currentQuestion).addClass( "noteattemptcardinvisible" )
-        //
-        var idanswercard = _makeCurrentQuestionAnswerId();
+
+        //Hide current question answer text for next use
+        //NB: we do this by removing both visibility control
+        //classes and then adding back the 'invisible' one
+        var idanswercard = _generateCurrentQuestionAnswerId();
         $('#' + idanswercard).removeClass( "answertextvisible" )
         $('#' + idanswercard).removeClass( "answertextinvisible" )
-        //
         $('#' + idanswercard).addClass( "answertextinvisible" )
       }
+      //Get next question to be asked
       TCG.LIL.currentQuestion = TCG.LIL.arrdiv[_getNextQuestion()];
-
+      console.log("TCG.LIL.currentQuestion is now " + TCG.LIL.currentQuestion);
+      //Reveal what is now the current question
       $('#' + TCG.LIL.currentQuestion).removeClass( "noteattemptcardinvisible" )
       $('#' + TCG.LIL.currentQuestion).addClass( "noteattemptcardvisible" )
     }
     function _getNextQuestion(){
       return _getRandomInt(TCG.LIL.arrdiv.length)
     }
-    function _tellMe(){
-      var idanswercard = _makeCurrentQuestionAnswerId();
-
-      $('#' + idanswercard).removeClass( "answertextinvisible" )
+    function _tellAnswerButtonClickHandler(){
+      //Reveal answer
+      var idanswercard = _generateCurrentQuestionAnswerId();
       $('#' + idanswercard).addClass( "answertextvisible" )
+      $('#' + idanswercard).removeClass( "answertextinvisible" )
+      //Disable 'tell me' and enable 'next'
+      $('#' + TCG.LIL.currentQuestion).find('.tellmepitch').addClass( "disabled" )
+      $('#' + TCG.LIL.currentQuestion).find('.nextpitch').removeClass( "disabled" )
     }
-    function _makeCurrentQuestionAnswerId(){
+    function _generateCurrentQuestionAnswerId(){
       return TCG.LIL.currentQuestion + '-answertext';
+    }
+    function reportDiagnostics(){
+      console.log("This is pageElementsInitialization. v2");
+      console.log(TCG.LIL.arrdiv);
+      console.log(TCG.LIL.arrdiv.length);
+      console.log(_getRandomInt(TCG.LIL.arrdiv.length));
     }
     return {
       //PUBLIC AREA
       currentQuestion : "",
+      //
       arrdiv : _buildArrayOfDivs(),
+      //
       pageElementsInitialization : function () {
-        console.log("This is pageElementsInitialization. v2");
-        console.log(TCG.LIL.arrdiv);
-        console.log(TCG.LIL.arrdiv.length);
-        console.log(_getRandomInt(TCG.LIL.arrdiv.length));
         $( ".tellmepitch" ).click(function() {
-          console.log( "Handler for tellmepitch .click() called." );
-          _tellMe()
+          _tellAnswerButtonClickHandler()
         });
         $( ".nextpitch" ).click(function() {
-          console.log( "Handler for nextpitch .click() called." );
-          _moveToNextQuestion();
+          _nextButtonClickHandler();
         });
         $( ".startquestions" ).click(function() {
           _start();
-          _moveToNextQuestion();
+          _nextButtonClickHandler();
+        });
+        //
+        $(document).bind('keydown', 't', function assets() {
+          console.log('t was pressed');
+          _tellAnswerButtonClickHandler()
+          return false;
+        });
+        $(document).bind('keydown', 'n', function assets() {
+          console.log('n was pressed');
+          _nextButtonClickHandler();
+          return false;
+        });
+        $(document).bind('keydown', 's', function assets() {
+          console.log('n was pressed');
+          _start();
+          _nextButtonClickHandler();
+          return false;
         });
       },
   };
